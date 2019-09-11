5ba5de1fb6d92431d11981cff9e011a83daf52c0
reidaruss <reidarussell@gmail.com>
2019-09-11 13:29:54 -0500

    Added temporary tests of insertionsort and bubblesort for each dataset

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,12 +2,40 @@
 #include "bubble.h"
 #include "insertion.h"
 
+//remove
+#include <fstream>
+#include <vector>
+//remove
 
 int main()
 {
 
     //datagen data;
     //data.execute();
+
+    //REMOVE
+    ifstream infile;
+    //infile.open("../lab1/data/unsorted/thousand_random.txt");
+    //infile.open("../lab1/data/unsorted/thousand_partUni.txt");
+    //infile.open("../lab1/data/unsorted/thousand_partRand.txt");
+    //infile.open("../lab1/data/unsorted/thousand_reverse.txt");
+    int size;
+    vector<int> arr;
+    infile >> size;
+    for(int i = 0; i < size; i++)
+    {
+        int num;
+        infile >> num;
+        arr.push_back(num);
+    }
+    cout << "BUBBLE:::::::::" <<endl;
+    Bubble b;
+    b.execute(arr);
+    cout << "INSERTION::::::::::::::::::::::::::::::::::::::::" << endl;
+    Insertion i;
+    i.execute(arr);
+    //REMOVE
+
     return 0;
 }
 
