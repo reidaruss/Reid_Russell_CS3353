0cbd3f1a95fdee4a3ef4a807c78996ac2d7c819b
Reid Russell <reidaruss@Reids-MacBook-Pro.local>
2019-09-24 13:34:40 -0500

    Added Lab 2 and updated file structure of repo

---
+++ lab1/src/cmake-build-debug/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/bubble.cpp.o"
+  "CMakeFiles/src.dir/datagen.cpp.o"
+  "CMakeFiles/src.dir/insertion.cpp.o"
+  "CMakeFiles/src.dir/main.cpp.o"
+  "CMakeFiles/src.dir/merge.cpp.o"
+  "CMakeFiles/src.dir/sort.cpp.o"
+  "src.pdb"
+  "src"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
