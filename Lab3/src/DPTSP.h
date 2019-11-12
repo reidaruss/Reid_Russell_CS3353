1fe164f7ddfa410758b59e65307963048449b020
reidaruss <reidarussell@gmail.com>
2019-11-11 23:35:25 -0600

    Just removed a bunch of old code that was commented out

--- Lab3/src/DPTSP.h
+++ Lab3/src/DPTSP.h
@@ -2,43 +2,6 @@
 // Created by Reid Russell on 11/5/19.
 //
 //
-//#ifndef LAB3_DPTSP_H
-//#define LAB3_DPTSP_H
-//
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//#include <cmath>
-//#include <chrono>
-//
-//#include "Node.h"
-//
-//
-//class dynamicProgTSP {
-//
-//private:
-//    float tCost;
-//
-//    std::vector<Node> nodes;
-//    std::vector<std::vector<float>> dist;
-//
-//    std::vector<float> subtrees;
-//    std::vector<std::vector<bool> > state;
-//
-//    std::vector<std::vector<Node> > path;
-//    std::vector<Node> fullPath;
-//
-//public:
-//    dynamicProgTSP(std::vector<Node>* nList);
-//    void calcDist();
-//    int findMin(int index);
-//    bool isVisited(int id);
-//    float tsp(int node, std::vector<bool> visited, std::vector<Node>& tempPath);
-//    void run();
-//
-//};
-
-
 
 
 #ifndef LAB3_DPTSP_H
